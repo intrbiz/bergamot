378bc75ee8cb621899f40ad405963f2998b7c7ce
Chris Ellis <chris@intrbiz.com>
2014-07-13 00:20:24 +0100

    Use ParameterisedMO interface

--- bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/check/ExecuteCheck.java
+++ bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/check/ExecuteCheck.java
@@ -1,6 +1,5 @@
 package com.intrbiz.bergamot.model.message.check;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
@@ -9,12 +8,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.intrbiz.bergamot.model.message.Message;
 import com.intrbiz.bergamot.model.message.ParameterMO;
+import com.intrbiz.bergamot.model.message.ParameterisedMO;
 
 /**
  * Execute this check please
  */
 @JsonTypeName("bergamot.execute_check")
-public class ExecuteCheck extends Message
+public class ExecuteCheck extends Message implements ParameterisedMO
 {
     @JsonProperty("engine")
     private String engine;
@@ -111,56 +111,16 @@ public class ExecuteCheck extends Message
         this.siteId = siteId;
     }
 
+    @Override
     public List<ParameterMO> getParameters()
     {
         return parameters;
     }
 
+    @Override
     public void setParameters(List<ParameterMO> parameters)
     {
         this.parameters = parameters;
-    }
-
-    public void addParameter(String name, String value)
-    {
-        this.parameters.add(new ParameterMO(name, value));
-    }
-
-    public void setParameter(String name, String value)
-    {
-        this.removeParameter(name);
-        this.addParameter(name, value);
-    }
-
-    public void removeParameter(String name)
-    {
-        for (Iterator<ParameterMO> i = this.parameters.iterator(); i.hasNext();)
-        {
-            if (name.equals(i.next().getName()))
-            {
-                i.remove();
-                break;
-            }
-        }
-    }
-
-    public void clearParameters()
-    {
-        this.parameters.clear();
-    }
-
-    public String getParameter(String name)
-    {
-        return this.getParameter(name, null);
-    }
-
-    public String getParameter(String name, String defaultValue)
-    {
-        for (ParameterMO parameter : this.parameters)
-        {
-            if (name.equals(parameter.getName())) return parameter.getValue();
-        }
-        return defaultValue;
     }
 
     public long getTimeout()
