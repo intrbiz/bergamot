4b3c96808527ad16f2c33099a18375513c0a18e3
Chris Ellis <chris@intrbiz.com>
2014-08-02 11:46:25 +0100

    Implement API authentication

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/api/APIRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/api/APIRouter.java
@@ -1,21 +1,27 @@
 package com.intrbiz.bergamot.ui.api;
 
+import com.intrbiz.Util;
 import com.intrbiz.balsa.engine.route.Router;
+import com.intrbiz.balsa.engine.security.GenericAuthenticationToken;
 import com.intrbiz.balsa.error.http.BalsaNotFound;
 import com.intrbiz.balsa.http.HTTP.HTTPStatus;
 import com.intrbiz.balsa.metadata.WithDataAdapter;
 import com.intrbiz.bergamot.data.BergamotDB;
-import com.intrbiz.bergamot.model.Site;
+import com.intrbiz.bergamot.model.Contact;
 import com.intrbiz.bergamot.ui.BergamotApp;
 import com.intrbiz.metadata.Any;
 import com.intrbiz.metadata.Before;
 import com.intrbiz.metadata.Catch;
 import com.intrbiz.metadata.JSON;
+import com.intrbiz.metadata.Order;
 import com.intrbiz.metadata.Prefix;
 
 @Prefix("/api/")
 public class APIRouter extends Router<BergamotApp>
 {   
+    /**
+     * Default global API 404 error handler
+     */
     @Catch(BalsaNotFound.class)
     @Any("**")
     @JSON(status = HTTPStatus.NotFound)
@@ -24,14 +30,26 @@ public class APIRouter extends Router<BergamotApp>
         return "Not found";
     }
     
-    @Any("**")
+    /**
+     * Authenticate an API Request based on the authentication 
+     * token given on the request (if any). 
+     */
     @Before
+    @Any("**")
+    @Order(10)
     @WithDataAdapter(BergamotDB.class)
-    public void lookupSite(BergamotDB db)
+    public void authenticateRequest(BergamotDB db)
     {
-        // we want to avoid a session for the API,
-        // so lookup the site
-        Site site = var("site", db.getSiteByName(request().getServerName()));
-        if (site == null) throw new BalsaNotFound("No Bergamot site is configured for the server name: " + request().getServerName());
+        // perform a token based request authentication
+        // we may already have the auth from the session, if shared with a UI session
+        if (! this.validPrincipal())
+        {
+            authenticateRequest(new GenericAuthenticationToken(Util.coalesceEmpty(cookie("bergamot.api.key"), param("key"))));
+        }
+        // assert that the contact is permitted API access
+        require(permission("api.access"));
+        // setup the site based on the authenticated principal
+        Contact contact = var("contact", currentPrincipal());
+        var("site", contact.getSite());
     }
 }
