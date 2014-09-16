37ae9a3dc26f5c1eca703624bad92a23a00e47f7
Chris Ellis <chris@intrbiz.com>
2014-09-16 16:56:56 +0100

    Add location admin ui

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/BergamotApp.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/BergamotApp.java
@@ -49,6 +49,7 @@ import com.intrbiz.bergamot.ui.router.TrapRouter;
 import com.intrbiz.bergamot.ui.router.admin.AdminRouter;
 import com.intrbiz.bergamot.ui.router.admin.CommandAdminRouter;
 import com.intrbiz.bergamot.ui.router.admin.ContactAdminRouter;
+import com.intrbiz.bergamot.ui.router.admin.LocationAdminRouter;
 import com.intrbiz.bergamot.ui.router.admin.TeamAdminRouter;
 import com.intrbiz.bergamot.ui.router.admin.TimePeriodAdminRouter;
 import com.intrbiz.bergamot.ui.security.BergamotSecurityEngine;
@@ -144,6 +145,7 @@ public class BergamotApp extends BalsaApplication implements Configurable<UICfg>
         router(new TeamAdminRouter());
         router(new TimePeriodAdminRouter());
         router(new CommandAdminRouter());
+        router(new LocationAdminRouter());
         // API
         router(new APIRouter());
         router(new MetricsAPIRouter());
---
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/admin/LocationAdminRouter.java
@@ -0,0 +1,58 @@
+package com.intrbiz.bergamot.ui.router.admin;
+
+import java.util.UUID;
+
+import com.intrbiz.Util;
+import com.intrbiz.balsa.engine.route.Router;
+import com.intrbiz.balsa.metadata.WithDataAdapter;
+import com.intrbiz.bergamot.config.model.LocationCfg;
+import com.intrbiz.bergamot.data.BergamotDB;
+import com.intrbiz.bergamot.model.Config;
+import com.intrbiz.bergamot.model.Site;
+import com.intrbiz.bergamot.ui.BergamotApp;
+import com.intrbiz.configuration.Configuration;
+import com.intrbiz.metadata.Any;
+import com.intrbiz.metadata.AsUUID;
+import com.intrbiz.metadata.Get;
+import com.intrbiz.metadata.Prefix;
+import com.intrbiz.metadata.RequirePermission;
+import com.intrbiz.metadata.RequireValidPrincipal;
+import com.intrbiz.metadata.SessionVar;
+import com.intrbiz.metadata.Template;
+
+@Prefix("/admin/location")
+@Template("layout/main")
+@RequireValidPrincipal()
+@RequirePermission("ui.admin")
+@RequirePermission("ui.admin.location")
+public class LocationAdminRouter extends Router<BergamotApp>
+{    
+    @Any("/")
+    @WithDataAdapter(BergamotDB.class)
+    public void index(BergamotDB db, @SessionVar("site") Site site)
+    {
+        model("locations", db.listLocations(site.getId()));
+        model("location_templates", db.listConfigTemplates(site.getId(), Configuration.getRootElement(LocationCfg.class)));
+        encode("admin/location/index");
+    }
+    
+    @Get("/configure/id/:id")
+    @WithDataAdapter(BergamotDB.class)
+    public void showConfigure(BergamotDB db, @AsUUID UUID timeperiodId, @SessionVar("site") Site site)
+    {
+        model("location", Util.nullable(db.getConfig(timeperiodId), Config::getResolvedConfiguration));
+        model("templates", db.listConfigTemplates(site.getId(), Configuration.getRootElement(LocationCfg.class)));
+        model("locations", db.listConfig(site.getId(), Configuration.getRootElement(LocationCfg.class)));
+        encode("admin/location/configure");
+    }
+    
+    @Get("/configure")
+    @WithDataAdapter(BergamotDB.class)
+    public void showConfigureNew(BergamotDB db, @SessionVar("site") Site site)
+    {
+        model("location", new LocationCfg());
+        model("templates", db.listConfigTemplates(site.getId(), Configuration.getRootElement(LocationCfg.class)));
+        model("locations", db.listConfig(site.getId(), Configuration.getRootElement(LocationCfg.class)));
+        encode("admin/location/configure");
+    }
+}
