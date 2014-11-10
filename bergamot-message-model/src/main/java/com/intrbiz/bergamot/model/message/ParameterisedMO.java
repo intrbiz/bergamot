6997a705071ea6fabe12f15ca910a843a0e5e74a
Chris Ellis <chris@intrbiz.com>
2014-11-10 23:48:00 +0000

    Add parameter accessor util methods

--- bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/ParameterisedMO.java
+++ bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/ParameterisedMO.java
@@ -1,6 +1,11 @@
 package com.intrbiz.bergamot.model.message;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.intrbiz.Util;
 
 public interface ParameterisedMO
 {
@@ -41,5 +46,181 @@ public interface ParameterisedMO
                 .findFirst()
                 .map(ParameterMO::getValue)
                 .orElse(defaultValue);
+    }
+    
+    default boolean containsParameter(String name)
+    {
+        return this.getParameter(name) != null;
+    }
+    
+    default List<ParameterMO> getParameters(String name)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return name.equals(p.getName());})
+                .collect(Collectors.toList());
+    }
+    
+    default List<ParameterMO> getParametersStartingWith(final String prefix)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return p.getName().startsWith(prefix);})
+                .collect(Collectors.toList());
+    }
+    
+    default List<String> getParametersStartingWithValues(final String prefix)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return p.getName().startsWith(prefix);})
+                .map(ParameterMO::getValue)
+                .collect(Collectors.toList());
+    }
+    
+    default int getIntParameter(String name)
+    {
+        return this.getIntParameter(name, -1);
+    }
+    
+    default int getIntParameter(String name, int defaultValue)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return name.equals(p.getName());})
+                .findFirst()
+                .map(ParameterMO::getValue)
+                .map(Integer::parseInt)
+                .orElse(defaultValue);        
+    }
+    
+    default long getLongParameter(String name)
+    {
+        return this.getLongParameter(name, -1);
+    }
+    
+    default long getLongParameter(String name, long defaultValue)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return name.equals(p.getName());})
+                .findFirst()
+                .map(ParameterMO::getValue)
+                .map(Long::parseLong)
+                .orElse(defaultValue);        
+    }
+    
+    default double getDoubleParameter(String name)
+    {
+        return this.getDoubleParameter(name, -1);
+    }
+    
+    default double getDoubleParameter(String name, double defaultValue)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return name.equals(p.getName());})
+                .findFirst()
+                .map(ParameterMO::getValue)
+                .map(Double::parseDouble)
+                .orElse(defaultValue);        
+    }
+    
+    default float getFloatParameter(String name)
+    {
+        return this.getFloatParameter(name, -1);
+    }
+    
+    default float getFloatParameter(String name, float defaultValue)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return name.equals(p.getName());})
+                .findFirst()
+                .map(ParameterMO::getValue)
+                .map(Float::parseFloat)
+                .orElse(defaultValue);        
+    }
+    
+    default boolean getBooleanParameter(String name)
+    {
+        return this.getBooleanParameter(name, false);
+    }
+    
+    default boolean getBooleanParameter(String name, boolean defaultValue)
+    {
+        return this.getParameters().stream()
+                .filter((p) -> {return name.equals(p.getName());})
+                .findFirst()
+                .map(ParameterMO::getValue)
+                .map((v) -> { return "yes".equalsIgnoreCase(v) || "true".equalsIgnoreCase(v) || "on".equalsIgnoreCase(v); })
+                .orElse(defaultValue);        
+    }
+    
+    
+    default Set<String> getParameterCSV(String name)
+    {
+        return this.getParameterCSV(name, null);
+    }
+    
+    default Set<String> getParameterCSV(String name, String defaultValue)
+    {
+        String value = this.getParameter(name, defaultValue);
+        if (value == null) return new LinkedHashSet<String>(); 
+        Set<String> r = new LinkedHashSet<String>();
+        for (String s : value.split(", ?"))
+        {
+            s = s.trim();
+            if (! Util.isEmpty(s))
+            {
+                r.add(s);
+            }
+        }
+        return r;
+    }
+    
+    default Set<Integer> getIntParameterCSV(String name)
+    {
+        return this.getIntParameterCSV(name, null);
+    }
+    
+    default Set<Integer> getIntParameterCSV(String name, String defaultValue)
+    {
+        return this.getParameterCSV(name, defaultValue)
+                .stream()
+                .map(Integer::parseInt)
+                .collect(LinkedHashSet::new, Set::add, Set::addAll);
+    }
+    
+    default Set<Long> getLongParameterCSV(String name)
+    {
+        return this.getLongParameterCSV(name, null);
+    }
+    
+    default Set<Long> getLongParameterCSV(String name, String defaultValue)
+    {
+        return this.getParameterCSV(name, defaultValue)
+                .stream()
+                .map(Long::parseLong)
+                .collect(LinkedHashSet::new, Set::add, Set::addAll);
+    }
+    
+    default Set<Double> getDoubleParameterCSV(String name)
+    {
+        return this.getDoubleParameterCSV(name, null);
+    }
+    
+    default Set<Double> getDoubleParameterCSV(String name, String defaultValue)
+    {
+        return this.getParameterCSV(name, defaultValue)
+                .stream()
+                .map(Double::parseDouble)
+                .collect(LinkedHashSet::new, Set::add, Set::addAll);
+    }
+    
+    default Set<Float> getFloatParameterCSV(String name)
+    {
+        return this.getFloatParameterCSV(name, null);
+    }
+    
+    default Set<Float> getFloatParameterCSV(String name, String defaultValue)
+    {
+        return this.getParameterCSV(name, defaultValue)
+                .stream()
+                .map(Float::parseFloat)
+                .collect(LinkedHashSet::new, Set::add, Set::addAll);
     }
 }
