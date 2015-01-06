80ca482eb284704c2f90fc702ec7938491dfec01
Chris Ellis <chris@intrbiz.com>
2015-01-06 22:24:16 +0000

    Add paging

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/StatsRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/StatsRouter.java
@@ -8,6 +8,9 @@ import com.intrbiz.bergamot.data.BergamotDB;
 import com.intrbiz.bergamot.ui.BergamotApp;
 import com.intrbiz.metadata.Any;
 import com.intrbiz.metadata.AsUUID;
+import com.intrbiz.metadata.CoalesceMode;
+import com.intrbiz.metadata.IsaLong;
+import com.intrbiz.metadata.Param;
 import com.intrbiz.metadata.Prefix;
 import com.intrbiz.metadata.RequireValidPrincipal;
 import com.intrbiz.metadata.Template;
@@ -19,10 +22,17 @@ public class StatsRouter extends Router<BergamotApp>
 {    
     @Any("/transitions/check/id/:id")
     @WithDataAdapter(BergamotDB.class)
-    public void trap(BergamotDB db, @AsUUID UUID id)
+    public void trap(
+            BergamotDB db, 
+            @AsUUID UUID id, 
+            @Param("offset") @IsaLong(min = 0, mandatory = true, coalesce = CoalesceMode.ALWAYS, defaultValue = 0L)   long offset,
+            @Param("limit")  @IsaLong(min = 1, mandatory = true, coalesce = CoalesceMode.ALWAYS, defaultValue = 25L) long limit
+    )
     {
         model("check", db.getCheck(id));
-        model("transitions", db.listCheckTransitionsForCheck(id, 0, 100));
+        model("transitions", db.listCheckTransitionsForCheck(id, offset, limit));
+        var("offset", offset);
+        var("limit", limit);
         encode("stats/transitions");
     }
 }
