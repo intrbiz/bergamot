e4e6d5c8b0074b3239afa13060699eece40b288f
Chris Ellis <chris@intrbiz.com>
2015-03-15 18:39:40 +0000

    Move to BC provider for reading certs

--- bergamot-crypto-util/src/main/java/com/intrbiz/bergamot/crypto/util/PEMUtil.java
+++ bergamot-crypto-util/src/main/java/com/intrbiz/bergamot/crypto/util/PEMUtil.java
@@ -1,27 +1,32 @@
 package com.intrbiz.bergamot.crypto.util;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
+import java.security.Security;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
 import java.security.spec.PKCS8EncodedKeySpec;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMReader;
 import org.bouncycastle.openssl.PEMWriter;
 import org.bouncycastle.util.io.pem.PemObject;
 import org.bouncycastle.util.io.pem.PemReader;
 
 public class PEMUtil
 {
+    static
+    {
+        // as much as I dislike this, we need the BC provider to load a PEM formatted cert reliably!
+        Security.addProvider(new BouncyCastleProvider());
+    }
+    
     public static String saveCertificate(Certificate cert)
     {
         StringWriter sw = new StringWriter();
@@ -53,29 +58,17 @@ public class PEMUtil
     
     public static Certificate loadCertificate(File file) throws IOException
     {
-        try (FileInputStream in = new FileInputStream(file))
+        try (PEMReader pr = new PEMReader(new FileReader(file)))
         {
-            CertificateFactory factory = CertificateFactory.getInstance("X.509");
-            Certificate cert =  factory.generateCertificate(in);
-            return cert;
-        }
-        catch (Exception e)
-        {
-            throw new IOException("Error loading certificate", e);
+            return (Certificate) pr.readObject();
         }
     }
     
     public static Certificate loadCertificate(String data) throws IOException
     {
-        try (InputStream in = new ByteArrayInputStream(data.getBytes()))
+        try (PEMReader pr = new PEMReader(new StringReader(data)))
         {
-            CertificateFactory factory = CertificateFactory.getInstance("X.509");
-            Certificate cert =  factory.generateCertificate(in);
-            return cert;
-        }
-        catch (Exception e)
-        {
-            throw new IOException("Error loading certificate", e);
+            return (Certificate) pr.readObject();
         }
     }
     
