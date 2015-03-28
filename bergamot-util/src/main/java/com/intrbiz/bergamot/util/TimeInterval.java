9c8fac4345a6c207080fc2a8c08561b7cb5d4474
Chris Ellis <chris@intrbiz.com>
2015-03-28 02:44:02 +0000

    Add hashcode and equals

--- bergamot-util/src/main/java/com/intrbiz/bergamot/util/TimeInterval.java
+++ bergamot-util/src/main/java/com/intrbiz/bergamot/util/TimeInterval.java
@@ -43,6 +43,28 @@ public final class TimeInterval
         return this.unit.toMillis(this.value);
     }
     
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((unit == null) ? 0 : unit.hashCode());
+        result = prime * result + (int) (value ^ (value >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        TimeInterval other = (TimeInterval) obj;
+        if (unit != other.unit) return false;
+        if (value != other.value) return false;
+        return true;
+    }
+
     public static TimeInterval milliseconds(long value)
     {
         return new TimeInterval(value, TimeUnit.MILLISECONDS);
