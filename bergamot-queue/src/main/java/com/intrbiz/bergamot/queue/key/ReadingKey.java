7b50d40850d78b4d6f89c9d1e924075d5eaf2f38
Chris Ellis <chris@intrbiz.com>
2015-06-07 13:16:42 +0100

    Implement ReadingKey

--- bergamot-queue/src/main/java/com/intrbiz/bergamot/queue/key/ReadingKey.java
+++ bergamot-queue/src/main/java/com/intrbiz/bergamot/queue/key/ReadingKey.java
@@ -2,28 +2,31 @@ package com.intrbiz.bergamot.queue.key;
 
 import java.util.UUID;
 
+import com.intrbiz.bergamot.model.message.SiteMO;
 import com.intrbiz.queue.name.GenericKey;
 
 /**
  * A key used to route readings
  */
-public abstract class ReadingKey extends GenericKey
+public class ReadingKey extends GenericKey
 {    
     protected ReadingKey(String key)
     {
         super(key);
     }
     
-    /**
-     * For a given id mask down to only the site id.
-     * 
-     * Note: toSiteId(site_id) == site_id
-     * 
-     * @param id the id to mask
-     * @return the site id
-     */
-    public static UUID toSiteId(UUID id)
+    public ReadingKey(UUID siteId, int pool)
     {
-        return new UUID((id.getMostSignificantBits() & 0xFFFFFFFF_FFFF0000L) | 0x0000000000004000L, 0x80000000_00000000L);
+        super(SiteMO.getSiteId(siteId) + "." + pool);
+    }
+    
+    public ReadingKey(UUID checkId)
+    {
+        super(SiteMO.getSiteId(checkId) + "." + SiteMO.getProcessingPool(checkId));
+    }
+    
+    public ReadingKey()
+    {
+        super("");
     }
 }
