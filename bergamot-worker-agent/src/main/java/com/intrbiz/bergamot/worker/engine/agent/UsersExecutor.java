505c1967ce7fd31b5f5d9345b0458ed9ac2a26a9
Chris Ellis <chris@intrbiz.com>
2015-06-15 23:15:02 +0100

    Add active-users reading

--- bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/UsersExecutor.java
+++ bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/UsersExecutor.java
@@ -9,9 +9,12 @@ import com.intrbiz.bergamot.agent.server.BergamotAgentServerHandler;
 import com.intrbiz.bergamot.model.message.agent.check.CheckWho;
 import com.intrbiz.bergamot.model.message.agent.stat.WhoStat;
 import com.intrbiz.bergamot.model.message.check.ExecuteCheck;
+import com.intrbiz.bergamot.model.message.reading.ReadingParcelMO;
 import com.intrbiz.bergamot.model.message.result.ActiveResultMO;
 import com.intrbiz.bergamot.model.message.result.ResultMO;
+import com.intrbiz.bergamot.queue.key.ReadingKey;
 import com.intrbiz.bergamot.worker.engine.AbstractExecutor;
+import com.intrbiz.gerald.polyakov.gauge.IntegerGaugeReading;
 
 
 /**
@@ -63,6 +66,10 @@ public class UsersExecutor extends AbstractExecutor<AgentEngine>
                             executeCheck.getIntParameter("critical", 5), 
                             stat.getUsers().size() + " active users"
                     ).runtime(runtime));
+                    // readings
+                    ReadingParcelMO readings = new ReadingParcelMO().fromCheck(executeCheck.getId()).captured(System.currentTimeMillis());
+                    readings.reading(new IntegerGaugeReading("active-users", null, stat.getUsers().size(), executeCheck.getIntParameter("warning",  2), executeCheck.getIntParameter("critical", 5), 0, Integer.MAX_VALUE));
+                    this.publishReading(new ReadingKey(executeCheck.getSiteId(), executeCheck.getProcessingPool()), readings);
                 });
             }
             else
