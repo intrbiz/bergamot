04fc5062690c09377538660c096589d5d6ca94cb
Chris Ellis <chris@intrbiz.com>
2015-06-16 22:39:58 +0100

    Add get by date API call

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/api/LamplighterAPIRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/api/LamplighterAPIRouter.java
@@ -1,6 +1,7 @@
 package com.intrbiz.bergamot.ui.api;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
@@ -17,8 +18,10 @@ import com.intrbiz.lamplighter.data.LamplighterDB;
 import com.intrbiz.lamplighter.model.CheckReading;
 import com.intrbiz.lamplighter.model.StoredDoubleGaugeReading;
 import com.intrbiz.metadata.Any;
+import com.intrbiz.metadata.CheckRegEx;
 import com.intrbiz.metadata.CoalesceMode;
 import com.intrbiz.metadata.IsaInt;
+import com.intrbiz.metadata.IsaLong;
 import com.intrbiz.metadata.JSON;
 import com.intrbiz.metadata.Prefix;
 import com.intrbiz.metadata.RequireValidPrincipal;
@@ -153,6 +156,62 @@ public class LamplighterAPIRouter extends Router<BergamotApp>
         jenny.writeEndArray();
         jenny.writeEndObject();
         */
+        // end y sets
+        jenny.writeEndArray();
+        jenny.writeEndObject();
+    }
+    
+    @Any("/graph/reading/gauge/double/:id/date/:rollup/:agg/:start/:end")
+    @WithDataAdapter(LamplighterDB.class)
+    public void getDoubleReadingsByDate(
+            LamplighterDB db, 
+            @Var("site") Site site, 
+            @IsaObjectId(session = false) UUID id, 
+            @CheckRegEx(value="(minute|hour|day|month)", mandatory = true, defaultValue = "hour", coalesce = CoalesceMode.ALWAYS) String rollup, 
+            @CheckRegEx(value="(avg|sum)", mandatory = true, defaultValue = "avg", coalesce = CoalesceMode.ALWAYS) String agg,
+            @IsaLong() Long start,
+            @IsaLong() Long end
+    ) throws IOException
+    {
+        CheckReading checkReading = db.getCheckReading(id);
+        List<StoredDoubleGaugeReading> readings = db.getDoubleGaugeReadingsByDate(checkReading.getSiteId(), checkReading.getId(), new Timestamp(start), new Timestamp(end), rollup, agg);
+        //
+        JsonGenerator jenny = response().ok().json().getJsonWriter();
+        jenny.writeStartObject();
+        // title
+        jenny.writeFieldName("title");
+        jenny.writeString(checkReading.getSummary());
+        // x-title
+        jenny.writeFieldName("x-title");
+        jenny.writeString("");
+        // y-title
+        jenny.writeFieldName("y-title");
+        jenny.writeString("");
+        // x
+        jenny.writeFieldName("x");
+        jenny.writeStartArray();
+        for (StoredDoubleGaugeReading reading : readings)
+        {
+            jenny.writeNumber(reading.getCollectedAt().getTime());
+        }
+        jenny.writeEndArray();
+        // y sets
+        jenny.writeFieldName("y");
+        jenny.writeStartArray();
+        // value
+        jenny.writeStartObject();
+        jenny.writeFieldName("title");
+        jenny.writeString(checkReading.getSummary() + (Util.isEmpty(checkReading.getUnit()) ? "" : " (" + checkReading.getUnit() + ")"));
+        jenny.writeFieldName("colour");
+        jenny.writeString("#FF0000");
+        jenny.writeFieldName("y");
+        jenny.writeStartArray();
+        for (StoredDoubleGaugeReading reading : readings)
+        {
+            jenny.writeNumber(reading.getValue());
+        }
+        jenny.writeEndArray();
+        jenny.writeEndObject();
         // end y sets
         jenny.writeEndArray();
         jenny.writeEndObject();
