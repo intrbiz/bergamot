2206c11b1e79cd96f3035d200174ab790d30566f
Chris Ellis <chris@intrbiz.com>
2015-06-21 21:13:15 +0100

    Add connections readings

--- bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/NetConExecutor.java
+++ bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/NetConExecutor.java
@@ -9,9 +9,12 @@ import com.intrbiz.bergamot.agent.server.BergamotAgentServerHandler;
 import com.intrbiz.bergamot.model.message.agent.check.CheckNetCon;
 import com.intrbiz.bergamot.model.message.agent.stat.NetConStat;
 import com.intrbiz.bergamot.model.message.check.ExecuteCheck;
+import com.intrbiz.bergamot.model.message.reading.ReadingParcelMO;
 import com.intrbiz.bergamot.model.message.result.ActiveResultMO;
 import com.intrbiz.bergamot.model.message.result.ResultMO;
+import com.intrbiz.bergamot.queue.key.ReadingKey;
 import com.intrbiz.bergamot.worker.engine.AbstractExecutor;
+import com.intrbiz.gerald.polyakov.gauge.IntegerGaugeReading;
 
 
 
@@ -78,6 +81,10 @@ public class NetConExecutor extends AbstractExecutor<AgentEngine>
                             executeCheck.getIntRangeParameter("critical", new Integer[] {1, 1}), 
                             "found " + stat.getConnections().size() + " connections"
                     ).runtime(runtime));
+                    // readings
+                    ReadingParcelMO readings = new ReadingParcelMO().fromCheck(executeCheck.getCheckId()).captured(System.currentTimeMillis());
+                    readings.reading(new IntegerGaugeReading("connections", null, stat.getConnections().size()));
+                    this.publishReading(new ReadingKey(executeCheck.getSiteId(), executeCheck.getProcessingPool()), readings);
                 });
             }
             else
