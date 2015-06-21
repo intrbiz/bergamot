64993d5cb12af0c1c4fb593c4ba50febc1185bd8
Chris Ellis <chris@intrbiz.com>
2015-06-21 21:24:24 +0100

    Add disk io readings

--- bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/DiskIOExecutor.java
+++ bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/DiskIOExecutor.java
@@ -10,10 +10,14 @@ import org.apache.log4j.Logger;
 import com.intrbiz.bergamot.agent.server.BergamotAgentServerHandler;
 import com.intrbiz.bergamot.model.message.agent.check.CheckDiskIO;
 import com.intrbiz.bergamot.model.message.agent.stat.DiskIOStat;
+import com.intrbiz.bergamot.model.message.agent.stat.diskio.DiskIOInfo;
 import com.intrbiz.bergamot.model.message.check.ExecuteCheck;
+import com.intrbiz.bergamot.model.message.reading.ReadingParcelMO;
 import com.intrbiz.bergamot.model.message.result.ActiveResultMO;
 import com.intrbiz.bergamot.model.message.result.ResultMO;
+import com.intrbiz.bergamot.queue.key.ReadingKey;
 import com.intrbiz.bergamot.worker.engine.AbstractExecutor;
+import com.intrbiz.gerald.polyakov.gauge.DoubleGaugeReading;
 
 
 /**
@@ -67,19 +71,36 @@ public class DiskIOExecutor extends AbstractExecutor<AgentEngine>
                     double runtime = ((double)(System.nanoTime() - sent)) / 1000_000D;
                     DiskIOStat stat = (DiskIOStat) response;
                     if (logger.isTraceEnabled()) logger.trace("Got DiskIOStat in " + runtime + "ms: " + stat);
+                    // thresholds
+                    double warning = executeCheck.getDoubleParameter("warning",  200);
+                    double critical = executeCheck.getDoubleParameter("critical", 250);
                     // apply the check
                     boolean peak = executeCheck.getBooleanParameter("peak", false);
                     resultSubmitter.accept(new ActiveResultMO().fromCheck(executeCheck).applyThresholds(
                             stat.getDisks(),
                             (v, t) -> (peak ? v.getFiveMinuteRate().getReadPeakRateMBps(): v.getFiveMinuteRate().getReadRateMBps()) > t || (peak ? v.getFiveMinuteRate().getWritePeakRateMBps(): v.getFiveMinuteRate().getWriteRateMBps()) > t,
-                            executeCheck.getDoubleParameter("warning",  200), 
-                            executeCheck.getDoubleParameter("critical", 250), 
+                            warning, 
+                            critical, 
                             stat.getDisks().stream().map((n) -> 
                                 n.getName() + 
                                 " Read: " + DFMT.format(n.getFiveMinuteRate().getReadRateMBps()) + "MB/s (" + DFMT.format(n.getFiveMinuteRate().getReadPeakRateMBps()) + "MB/s Peak) - " + DFMT.format(n.getFiveMinuteRate().getReads()) + "/s (" + DFMT.format(n.getFiveMinuteRate().getPeakReads()) + "/s Peak)" + 
                                 " Write: " + DFMT.format(n.getFiveMinuteRate().getWriteRateMBps()) + "MB/s (" + DFMT.format(n.getFiveMinuteRate().getWritePeakRateMBps()) + "MB/s Peak) - " + DFMT.format(n.getFiveMinuteRate().getWrites()) + "/s (" + DFMT.format(n.getFiveMinuteRate().getPeakWrites()) + "/s Peak)"
                             ).collect(Collectors.joining("; "))
                     ).runtime(runtime));
+                    // readings
+                    ReadingParcelMO readings = new ReadingParcelMO().fromCheck(executeCheck.getCheckId()).captured(System.currentTimeMillis());
+                    for (DiskIOInfo disk : stat.getDisks())
+                    {
+                        // rate
+                        readings.reading(new DoubleGaugeReading("read-rate-[" + disk.getName() + "]", "MB/s", disk.getFiveMinuteRate().getReadRateMBps(), warning, critical, null, null));
+                        readings.reading(new DoubleGaugeReading("read-rate-peak-[" + disk.getName() + "]", "MB/s", disk.getFiveMinuteRate().getReadPeakRateMBps(), warning, critical, null, null));
+                        readings.reading(new DoubleGaugeReading("write-rate-[" + disk.getName() + "]", "MB/s", disk.getFiveMinuteRate().getWriteRateMBps(), warning, critical, null, null));
+                        readings.reading(new DoubleGaugeReading("write-rate-peak-[" + disk.getName() + "]", "MB/s", disk.getFiveMinuteRate().getWritePeakRateMBps(), warning, critical, null, null));
+                        // reads
+                        readings.reading(new DoubleGaugeReading("reads-[" + disk.getName() + "]", null, disk.getFiveMinuteRate().getReads(), warning, critical, null, null));
+                        readings.reading(new DoubleGaugeReading("writes-[" + disk.getName() + "]", null, disk.getFiveMinuteRate().getWrites(), warning, critical, null, null));
+                    }
+                    this.publishReading(new ReadingKey(executeCheck.getSiteId(), executeCheck.getProcessingPool()), readings);
                 });
             }
             else
