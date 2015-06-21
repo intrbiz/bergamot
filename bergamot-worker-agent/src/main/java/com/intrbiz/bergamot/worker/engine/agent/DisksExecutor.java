42a1c430cb80561fec04000d506dd387d1085a3b
Chris Ellis <chris@intrbiz.com>
2015-06-21 20:55:10 +0100

    Add disks readings

--- bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/DisksExecutor.java
+++ bergamot-worker-agent/src/main/java/com/intrbiz/bergamot/worker/engine/agent/DisksExecutor.java
@@ -14,10 +14,13 @@ import com.intrbiz.bergamot.model.message.agent.check.CheckDisk;
 import com.intrbiz.bergamot.model.message.agent.stat.DiskStat;
 import com.intrbiz.bergamot.model.message.agent.stat.disk.DiskInfo;
 import com.intrbiz.bergamot.model.message.check.ExecuteCheck;
+import com.intrbiz.bergamot.model.message.reading.ReadingParcelMO;
 import com.intrbiz.bergamot.model.message.result.ActiveResultMO;
 import com.intrbiz.bergamot.model.message.result.ResultMO;
+import com.intrbiz.bergamot.queue.key.ReadingKey;
 import com.intrbiz.bergamot.util.UnitUtil;
 import com.intrbiz.bergamot.worker.engine.AbstractExecutor;
+import com.intrbiz.gerald.polyakov.gauge.DoubleGaugeReading;
 
 
 
@@ -66,14 +69,25 @@ public class DisksExecutor extends AbstractExecutor<AgentEngine>
                     DiskStat stat = (DiskStat) response;
                     if (logger.isTraceEnabled()) logger.trace("Got Disk usage in + " + runtime + "ms: " + stat);
                     // apply the check
+                    double warning = executeCheck.getPercentParameter("warning", 0.8D);
+                    double critical = executeCheck.getPercentParameter("critical", 0.9D);
                     resultSubmitter.accept(new ActiveResultMO().fromCheck(executeCheck).applyGreaterThanThresholds(
                             stat.getDisks().stream().map(DiskInfo::getUsedPercent).map(UnitUtil::fromPercent).collect(Collectors.toList()),
-                            executeCheck.getPercentParameter("warning", 0.8F),
-                            executeCheck.getPercentParameter("critical", 0.9F),
+                            warning,
+                            critical,
                             "Disks: " + stat.getDisks().stream().map((disk)-> {
                                 return "" + disk.getMount() + " " + DFMT.format(toG(disk.getUsed())) + " GB of " + DFMT.format(toG(disk.getSize())) + " GB (" + DFMT.format(disk.getUsedPercent()) + " %) used";
                             }).collect(Collectors.joining("; "))
                     ).runtime(runtime));
+                    // readings
+                    ReadingParcelMO readings = new ReadingParcelMO().fromCheck(executeCheck.getCheckId()).captured(System.currentTimeMillis());
+                    for (DiskInfo disk : stat.getDisks())
+                    {
+                        readings.reading(new DoubleGaugeReading("disk-space-used-[" + disk.getMount() + "]", "MB", UnitUtil.toM(disk.getUsed()), UnitUtil.toM(disk.getSize()) * warning, UnitUtil.toM(disk.getSize()) * critical, 0D, UnitUtil.toM(disk.getSize())));
+                        readings.reading(new DoubleGaugeReading("disk-space-available-[" + disk.getMount() + "]", "MB", UnitUtil.toM(disk.getAvailable()), UnitUtil.toM(disk.getSize()) * (1D - warning), UnitUtil.toM(disk.getSize()) * (1D - critical), 0D, UnitUtil.toM(disk.getSize()) ));
+                        readings.reading(new DoubleGaugeReading("disk-space-used-percent-[" + disk.getMount() + "]", "%", disk.getUsedPercent(), UnitUtil.toPercent(warning), UnitUtil.toPercent(critical), 0D, 100D));
+                    }
+                    this.publishReading(new ReadingKey(executeCheck.getSiteId(), executeCheck.getProcessingPool()), readings);
                 });
             }
             else
