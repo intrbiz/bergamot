644d57839269743114e1c439fbed1f09d8488549
Chris Ellis <chris@intrbiz.com>
2015-06-21 01:14:36 +0100

    C&P error

--- bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/cluster/manager/response/InitedSite.java
+++ bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/cluster/manager/response/InitedSite.java
@@ -1,32 +1,13 @@
 package com.intrbiz.bergamot.model.message.cluster.manager.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.intrbiz.bergamot.model.message.cluster.manager.ClusterManagerResponse;
 
 @JsonTypeName("bergamot.cluster.manager.inited_site")
 public class InitedSite extends ClusterManagerResponse
-{
-    @JsonProperty("certificate_pem")
-    private String certificatePEM;
-    
+{    
     public InitedSite()
     {
         super();
-    }
-    
-    public InitedSite(String certificatePEM)
-    {
-        this.certificatePEM = certificatePEM;
-    }
-
-    public String getCertificatePEM()
-    {
-        return certificatePEM;
-    }
-
-    public void setCertificatePEM(String certificatePEM)
-    {
-        this.certificatePEM = certificatePEM;
     }
 }
