17482e0bf413a8f97bc0bcf2087d2ab6098e91cf
Chris Ellis <chris@intrbiz.com>
2015-08-07 19:19:58 +0100

    Move location API to new permissions model

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/api/LocationAPIRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/api/LocationAPIRouter.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import com.intrbiz.Util;
 import com.intrbiz.balsa.engine.route.Router;
-import com.intrbiz.balsa.error.http.BalsaNotFound;
 import com.intrbiz.balsa.metadata.WithDataAdapter;
 import com.intrbiz.bergamot.config.model.LocationCfg;
 import com.intrbiz.bergamot.data.BergamotDB;
@@ -31,110 +29,117 @@ public class LocationAPIRouter extends Router<BergamotApp>
 {
     @Get("/")
     @JSON
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public List<LocationMO> getLocations(BergamotDB db, @Var("site") Site site)
     {
-        return db.listLocations(site.getId()).stream().map(Location::toStubMO).collect(Collectors.toList());
+        return db.listLocations(site.getId()).stream().filter((l) -> permission("read", l)).map(Location::toStubMO).collect(Collectors.toList());
     }
     
     @Get("/roots")
     @JSON
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public List<LocationMO> getRootLocations(BergamotDB db, @Var("site") Site site)
     {
-        return db.getRootLocations(site.getId()).stream().filter((e)->{return e.getLocation() == null;}).map(Location::toMO).collect(Collectors.toList());
+        return db.getRootLocations(site.getId()).stream().filter((l) -> permission("read", l)).map(Location::toMO).collect(Collectors.toList());
     }
     
     @Get("/name/:name")
     @JSON(notFoundIfNull = true)
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public LocationMO getLocation(BergamotDB db, @Var("site") Site site, String name)
     {
-        return Util.nullable(db.getLocationByName(site.getId(), name), Location::toMO);
+        Location location = notNull(db.getLocationByName(site.getId(), name));
+        require(permission("read", location));
+        return location.toMO();
     }
     
     @Get("/name/:name/children")
     @JSON(notFoundIfNull = true)
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public List<LocationMO> getLocationChildren(BergamotDB db, @Var("site") Site site, String name)
     {
-        return Util.nullable(db.getLocationByName(site.getId(), name), (e)->{return e.getChildren().stream().map(Location::toMO).collect(Collectors.toList());});
+        Location location = notNull(db.getLocationByName(site.getId(), name));
+        require(permission("read", location));
+        return location.getChildren().stream().filter((l) -> permission("read", l)).map(Location::toMO).collect(Collectors.toList());
     }
     
     @Get("/name/:name/hosts")
     @JSON(notFoundIfNull = true)
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public List<HostMO> getLocationHosts(BergamotDB db, @Var("site") Site site, String name)
     {
-        return Util.nullable(db.getLocationByName(site.getId(), name), (e)->{return e.getHosts().stream().map(Host::toMO).collect(Collectors.toList());});
+        Location location = notNull(db.getLocationByName(site.getId(), name));
+        require(permission("read", location));
+        return location.getHosts().stream().filter((h) -> permission("read", h)).map(Host::toMO).collect(Collectors.toList());
     }
     
     //
     
     @Get("/id/:id")
     @JSON(notFoundIfNull = true)
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public LocationMO getLocation(BergamotDB db, @IsaObjectId(session = false) UUID id)
     {
-        return Util.nullable(db.getLocation(id), Location::toMO);
+        Location location = notNull(db.getLocation(id));
+        require(permission("read", location));
+        return location.toMO();
     }
     
     @Get("/id/:id/children")
     @JSON(notFoundIfNull = true)
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public List<LocationMO> getLocationChildren(BergamotDB db, @IsaObjectId(session = false) UUID id)
     {
-        return Util.nullable(db.getLocation(id), (e)->{return e.getChildren().stream().map(Location::toMO).collect(Collectors.toList());});
+        Location location = notNull(db.getLocation(id));
+        require(permission("read", location));
+        return location.getChildren().stream().filter((l) -> permission("read", l)).map(Location::toMO).collect(Collectors.toList());
     }
     
     @Get("/id/:id/hosts")
     @JSON(notFoundIfNull = true)
-    @RequirePermission("api.read.location")
     @WithDataAdapter(BergamotDB.class)
     public List<HostMO> getLocationHosts(BergamotDB db, @IsaObjectId(session = false) UUID id)
     {
-        return Util.nullable(db.getLocation(id), (e)->{return e.getHosts().stream().map(Host::toMO).collect(Collectors.toList());});
+        Location location = notNull(db.getLocation(id));
+        require(permission("read", location));
+        return location.getHosts().stream().filter((h) -> permission("read", h)).map(Host::toMO).collect(Collectors.toList());
     }
     
     @Get("/id/:id/execute-all-hosts")
     @JSON()
-    @RequirePermission("api.write.host.execute")
     @WithDataAdapter(BergamotDB.class)
     public String executeHostsInLocation(BergamotDB db, @IsaObjectId(session = false) UUID id)
     { 
-        Location location = db.getLocation(id);
-        if (location == null) throw new BalsaNotFound("No location with id '" + id + "' exists.");
+        Location location = notNull(db.getLocation(id));
         int executed = 0;
         for (Host host : location.getHosts())
         {
-            action("execute-check", host);
-            executed++;
+            if (permission("execute", host))
+            {
+                action("execute-check", host);
+                executed++;
+            }
         }
         return "Ok, executed " + executed + " hosts";
     }
     
     @Get("/name/:name/config.xml")
     @XML(notFoundIfNull = true)
-    @RequirePermission("api.read.location.config")
     @WithDataAdapter(BergamotDB.class)
     public LocationCfg getLocationConfig(BergamotDB db, @Var("site") Site site, String name)
     {
-        return Util.nullable(db.getLocationByName(site.getId(), name), Location::getConfiguration);
+        Location location = notNull(db.getLocationByName(site.getId(), name));
+        require(permission("read.config", location));
+        return location.getConfiguration();
     }
     
     @Get("/id/:id/config.xml")
     @XML(notFoundIfNull = true)
-    @RequirePermission("api.read.location.config")
     @WithDataAdapter(BergamotDB.class)
     public LocationCfg getLocationConfig(BergamotDB db, @IsaObjectId(session = false) UUID id)
     {
-        return Util.nullable(db.getLocation(id), Location::getConfiguration);
+        Location location = notNull(db.getLocation(id));
+        require(permission("read.config", location));
+        return location.getConfiguration();
     }
 }
