cce3aa8be60219a44241c7c83dc7ae8f3ead971e
Chris Ellis <chris@intrbiz.com>
2015-08-08 16:43:41 +0100

    Start moving config admin to new permission

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/admin/ClusterAdminRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/admin/ClusterAdminRouter.java
@@ -1,5 +1,7 @@
 package com.intrbiz.bergamot.ui.router.admin;
 
+import java.util.stream.Collectors;
+
 import com.intrbiz.balsa.engine.route.Router;
 import com.intrbiz.balsa.metadata.WithDataAdapter;
 import com.intrbiz.bergamot.config.model.ClusterCfg;
@@ -24,8 +26,8 @@ public class ClusterAdminRouter extends Router<BergamotApp>
     @WithDataAdapter(BergamotDB.class)
     public void index(BergamotDB db, @SessionVar("site") Site site)
     {
-        model("clusters", db.listClusters(site.getId()));
-        model("cluster_templates", db.listConfigTemplates(site.getId(), Configuration.getRootElement(ClusterCfg.class)));
+        model("clusters", db.listClusters(site.getId()).stream().filter((c) -> permission("read.config", c.getId())).collect(Collectors.toList()));
+        model("cluster_templates", db.listConfigTemplates(site.getId(), Configuration.getRootElement(ClusterCfg.class)).stream().filter((c) -> permission("read.config", c.getId())).collect(Collectors.toList()));
         encode("admin/cluster/index");
     }
 }
