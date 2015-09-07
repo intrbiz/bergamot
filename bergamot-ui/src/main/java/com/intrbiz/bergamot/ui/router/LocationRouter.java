df3042458cd8410962af1bb3b38524bec5136cb4
Chris Ellis <chris@intrbiz.com>
2015-09-07 06:47:09 +0100

    Add create location GUI

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/LocationRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/LocationRouter.java
@@ -1,23 +1,36 @@
 package com.intrbiz.bergamot.ui.router;
 
+import static com.intrbiz.balsa.BalsaContext.*;
 import static com.intrbiz.bergamot.ui.util.Sorter.*;
 
 import java.io.IOException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.apache.log4j.Logger;
+
 import com.intrbiz.balsa.engine.route.Router;
 import com.intrbiz.balsa.metadata.WithDataAdapter;
+import com.intrbiz.bergamot.config.model.BergamotCfg;
 import com.intrbiz.bergamot.config.model.LocationCfg;
+import com.intrbiz.bergamot.config.model.TemplatedObjectCfg.ObjectState;
 import com.intrbiz.bergamot.data.BergamotDB;
+import com.intrbiz.bergamot.importer.BergamotImportReport;
 import com.intrbiz.bergamot.metadata.IsaObjectId;
+import com.intrbiz.bergamot.model.Config;
+import com.intrbiz.bergamot.model.ConfigChange;
+import com.intrbiz.bergamot.model.Contact;
 import com.intrbiz.bergamot.model.Host;
 import com.intrbiz.bergamot.model.Location;
 import com.intrbiz.bergamot.model.Site;
 import com.intrbiz.bergamot.ui.BergamotApp;
 import com.intrbiz.configuration.Configuration;
 import com.intrbiz.metadata.Any;
+import com.intrbiz.metadata.CheckStringLength;
+import com.intrbiz.metadata.CurrentPrincipal;
 import com.intrbiz.metadata.Get;
+import com.intrbiz.metadata.Param;
+import com.intrbiz.metadata.Post;
 import com.intrbiz.metadata.Prefix;
 import com.intrbiz.metadata.RequireValidPrincipal;
 import com.intrbiz.metadata.SessionVar;
@@ -28,6 +41,7 @@ import com.intrbiz.metadata.Template;
 @RequireValidPrincipal()
 public class LocationRouter extends Router<BergamotApp>
 {
+    private Logger logger = Logger.getLogger(LocationRouter.class);
     
     @Any("/")
     @WithDataAdapter(BergamotDB.class)
@@ -77,5 +91,64 @@ public class LocationRouter extends Router<BergamotApp>
         var("templates", db.listConfigTemplates(site.getId(), Configuration.getRootElement(LocationCfg.class)).stream().filter((t) -> permission("read", t.getId())).sorted((a, b) -> a.getSummary().compareTo(b.getSummary())).collect(Collectors.toList()));
         var("locations", db.listLocations(site.getId()).stream().filter((l) -> permission("read", l)).sorted((a, b) -> a.getSummary().compareTo(b.getSummary())).collect(Collectors.toList()));
         encode("/location/create");
+    }
+    
+    @Post("/create")
+    @WithDataAdapter(BergamotDB.class)
+    public void doCreate(
+            BergamotDB db, 
+            @SessionVar("site") Site site,
+            @CurrentPrincipal() Contact user,
+            @Param("location_extends") @IsaObjectId(mandatory = false) UUID templateId,
+            @Param("location_summary") @CheckStringLength(mandatory = true, max = 255) String summary,
+            @Param("location_name") @CheckStringLength(mandatory = true, max = 255) String name,
+            @Param("location_location") @IsaObjectId(mandatory = false) UUID locationId,
+            @Param("location_description") @CheckStringLength(mandatory = false, max = 4096) String description,
+            @Param("location_workerpool") @CheckStringLength(mandatory = false, max = 255) String workerPool
+    )
+    {
+        // create the configuration object we are going to add
+        LocationCfg config = new LocationCfg();
+        config.setTemplate(false);
+        config.setObjectState(ObjectState.PRESENT);
+        config.setSummary(summary);
+        config.setName(name);
+        config.setWorkerPool(workerPool);
+        config.setDescription(description);
+        // extends
+        if (templateId != null)
+        {
+            Config extendsCfg = db.getConfig(templateId);
+            if (extendsCfg != null && extendsCfg.getConfiguration() instanceof LocationCfg)
+            {
+                config.getInheritedTemplates().add(extendsCfg.getConfiguration().getName());
+            }
+        }
+        // location
+        if (locationId != null)
+        {
+            Config locationCfg = db.getConfig(locationId);
+            if (locationCfg != null && locationCfg.getConfiguration() instanceof LocationCfg)
+            {
+                config.setLocation(locationCfg.getConfiguration().getName());
+            }
+        }
+        // the container
+        BergamotCfg configContainer = new BergamotCfg();
+        configContainer.setSite(site.getName());
+        configContainer.setSummary("Create location: " + name);
+        configContainer.getLocations().add(config);
+        logger.info("Creating location " + name + ":\n" + configContainer);
+        // create the configuration change
+        ConfigChange change = new ConfigChange(site.getId(), user, configContainer);
+        db.setConfigChange(change);
+        // apply the change
+        BergamotImportReport report = action("apply-config-change", site.getId(), change.getId(), Balsa().url(Balsa().path("/reset")), user);
+        logger.info("Created location " + name + " success=" + report.isSuccessful() + ":\n" + report.toString());
+        // success
+        var("report", report);
+        var("config", config);
+        var("change", change);
+        encode("location/created");
     }
 }
