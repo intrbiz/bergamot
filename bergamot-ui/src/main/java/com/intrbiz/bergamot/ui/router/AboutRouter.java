7cec26a788fa8f2de888f2eb02b4b06457edcfd1
Chris Ellis <chris@intrbiz.com>
2015-09-14 08:45:09 +0100

    Basic route introspection

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/AboutRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/AboutRouter.java
@@ -1,5 +1,9 @@
 package com.intrbiz.bergamot.ui.router;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.intrbiz.balsa.engine.impl.route.Route;
 import com.intrbiz.balsa.engine.route.Router;
 import com.intrbiz.bergamot.ui.BergamotApp;
 import com.intrbiz.metadata.Any;
@@ -13,10 +17,27 @@ import com.intrbiz.metadata.Template;
 public class AboutRouter extends Router<BergamotApp>
 {    
     @Any("/")
-    public void trap()
+    public void about()
     {
         var("bergamot_version",  BergamotApp.VERSION.NUMBER);
         var("bergamot_codename", BergamotApp.VERSION.CODE_NAME);
         encode("about");
+    }
+    
+    @Any("/routes")
+    public void routes()
+    {
+        List<Route> routes = var("routes", new LinkedList<Route>());
+        // introspect our routes
+        for (Router<?> router : app().getRouters())
+        {
+            String prefix = router.getPrefix();
+            for (Route route : Route.fromRouter(prefix, router))
+            {
+                if ((! route.isFilter()) && (! route.isExceptionHandler())) 
+                    routes.add(route);
+            }
+        }
+        encode("docs/routes");
     }
 }
