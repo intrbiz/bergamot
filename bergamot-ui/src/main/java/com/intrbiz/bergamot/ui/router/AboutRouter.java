99a369b38d05212a50cce5c1ed654a8e7aa3aeb3
Chris Ellis <chris@intrbiz.com>
2015-09-19 15:33:54 +0100

    Generate API bindings

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/AboutRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/AboutRouter.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import com.intrbiz.Util;
 import com.intrbiz.balsa.engine.impl.route.Route;
@@ -13,10 +15,14 @@ import com.intrbiz.balsa.engine.impl.route.exec.argument.ArgumentBuilder;
 import com.intrbiz.balsa.engine.impl.route.exec.argument.ListParameterArgument;
 import com.intrbiz.balsa.engine.impl.route.exec.argument.ParameterArgument;
 import com.intrbiz.balsa.engine.route.Router;
+import com.intrbiz.balsa.util.BalsaWriter;
+import com.intrbiz.bergamot.metadata.IgnoreBinding;
 import com.intrbiz.bergamot.ui.BergamotApp;
 import com.intrbiz.metadata.Any;
+import com.intrbiz.metadata.ListOf;
 import com.intrbiz.metadata.Prefix;
 import com.intrbiz.metadata.RequireValidPrincipal;
+import com.intrbiz.metadata.SetOf;
 import com.intrbiz.metadata.Template;
 
 @Prefix("/about")
@@ -29,7 +35,73 @@ public class AboutRouter extends Router<BergamotApp>
     {
         var("bergamot_version",  BergamotApp.VERSION.NUMBER);
         var("bergamot_codename", BergamotApp.VERSION.CODE_NAME);
-        encode("about");
+        encode("about/index");
+    }
+    
+    @Any("/generate/api/bindings/java")
+    public void generateJavaAPIBindings() throws Exception
+    {
+        StringBuilder bindings = new StringBuilder();
+        Set<String> imports = new TreeSet<String>();
+        // generate the bindings
+        for (Router<?> router : app().getRouters())
+        {
+            String prefix = router.getPrefix();
+            if (prefix.startsWith("/api"))
+            {
+                for (Route route : Route.fromRouter(prefix, router))
+                {
+                    if ((! route.isFilter()) && (! route.isExceptionHandler()))
+                    {
+                        RouteInfo info = new RouteInfo(route);
+                        if (! info.isIgnoreBinding())
+                        {
+                            info.buildRouteAPICall(bindings, imports);
+                        }
+                    }
+                }
+            }
+        }
+        // write the response
+        BalsaWriter writer = response().ok().contentType("text/java").getViewWriter();
+        // write the client
+        writer.write("package com.intrbiz.bergamot;\n");
+        writer.write("\n");
+        writer.write("import com.intrbiz.bergamot.credentials.*;\n");
+        // imports
+        for (String imported : imports)
+        {
+            writer.append("import ").append(imported).append(";\n");
+        }
+        writer.write("\n");
+        writer.write("public class BergamotClient extends BaseBergamotClient\n");
+        writer.write("{\n");
+        writer.write("\n");
+        writer.write("    public BergamotClient(String baseURL, ClientCredentials credentials)\n");
+        writer.write("    {\n");
+        writer.write("        super(baseURL, credentials);\n");
+        writer.write("    }\n");
+        writer.write("\n");
+        writer.write("    public BergamotClient(String baseURL, String username, String password)\n");
+        writer.write("    {\n");
+        writer.write("        super(baseURL, username, password);\n");
+        writer.write("    }\n");
+        writer.write("\n");
+        writer.write("    public BergamotClient(String baseURL, String token)\n");
+        writer.write("    {\n");
+        writer.write("        super(baseURL, token);\n");
+        writer.write("    }\n");
+        writer.write("\n");
+        writer.write("    public BergamotClient(String baseURL)\n");
+        writer.write("    {\n");
+        writer.write("        super(baseURL);\n");
+        writer.write("    }\n");
+        writer.write("\n");
+        // calls
+        writer.write(bindings.toString());
+        // done
+        writer.write("}\n");
+        writer.write("\n");
     }
     
     @Any("/routes")
@@ -46,7 +118,7 @@ public class AboutRouter extends Router<BergamotApp>
                     routes.add(new RouteInfo(route));
             }
         }
-        encode("docs/routes");
+        encode("about/routes");
     }
     
     public static class RouteInfo
@@ -63,7 +135,11 @@ public class AboutRouter extends Router<BergamotApp>
         
         private Class<?> returnType;
         
+        private Class<?> returnOfType;
+        
         private String callName;
+        
+        private boolean ignoreBinding;
         
         public RouteInfo(Route route) throws Exception
         {
@@ -74,6 +150,13 @@ public class AboutRouter extends Router<BergamotApp>
             this.parameters = new LinkedList<RouteParameter>();
             this.callName = route.getHandler().getName();
             this.returnType = route.getHandler().getReturnType();
+            // of type
+            ListOf loa = route.getHandler().getAnnotation(ListOf.class);
+            if (loa != null) this.returnOfType = loa.value();
+            SetOf soa = route.getHandler().getAnnotation(SetOf.class);
+            if (soa != null) this.returnOfType = soa.value();
+            // manual binding
+            this.ignoreBinding = route.getHandler().getAnnotation(IgnoreBinding.class) != null;
             // use the exec build to introspect the route
             ExecBuilder exec = ExecBuilder.build(route);
             // look at the annotations
@@ -109,6 +192,11 @@ public class AboutRouter extends Router<BergamotApp>
         {
             return pattern;
         }
+        
+        public String getPath()
+        {
+            return (this.prefix.endsWith("/") ? this.prefix.substring(0, this.prefix.length() -1) : this.prefix) + this.pattern;
+        }
 
         public LinkedHashSet<String> getAs()
         {
@@ -124,28 +212,49 @@ public class AboutRouter extends Router<BergamotApp>
         {
             return returnType;
         }
+        
+        public Class<?> getReturnOfType()
+        {
+            return this.returnOfType;
+        }
+        
+        public String getBindingReturnType()
+        {
+            if (void.class == this.returnType)
+            {
+                return "String";
+            }
+            else if (List.class == this.returnType)
+            {
+                return "List<" + (this.returnOfType == null ? "?" : this.returnOfType.getSimpleName()) + ">";
+            }
+            return this.returnType.getSimpleName();
+        }
 
         public String getCallName()
         {
             return callName;
         }
-
-        public String buildRouteAPICall()
+        
+        public boolean isIgnoreBinding()
         {
-            StringBuilder sb = new StringBuilder();
-            sb.append("package com.intrbiz.bergamot.call;\n");
-            sb.append("\n");
-            sb.append("import java.io.IOException;\n");
-            sb.append("import org.apache.http.client.fluent.*;\n");
-            sb.append("import com.intrbiz.bergamot.*;\n");
-            sb.append("import com.intrbiz.bergamot.model.message.*;\n");
+            return this.ignoreBinding;
+        }
+
+        public void buildRouteAPICall(StringBuilder sb, Set<String> imports)
+        {
+            imports.add("java.io.IOException");
+            imports.add("org.apache.http.client.fluent.*");
+            imports.add("com.intrbiz.bergamot.*");
+            imports.add("com.intrbiz.bergamot.model.message.*");
             for (RouteParameter param : this.parameters)
             {
-                sb.append("import ").append(param.getType().getCanonicalName()).append(";\n");    
+                imports.add(param.getType().getCanonicalName());    
             }
-            sb.append("import ").append(this.returnType.getCanonicalName()).append(";\n");
-            sb.append("\n");
-            sb.append("public class ").append(Util.ucFirst(this.callName)).append("Call extends BergamotAPICall<").append(this.returnType.getSimpleName()).append(">\n");
+            if (void.class != this.returnType) imports.add(this.returnType.getCanonicalName());
+            if (this.returnOfType != null) imports.add(this.returnOfType.getCanonicalName());
+            sb.append("\n\n");
+            sb.append("public static class ").append(Util.ucFirst(this.callName)).append("Call extends BergamotAPICall<").append(this.getBindingReturnType()).append(">\n");
             sb.append("{\n");
             // parameters
             for (RouteParameter parameter : this.parameters)
@@ -153,6 +262,13 @@ public class AboutRouter extends Router<BergamotApp>
                 sb.append("\n");
                 sb.append("    private ").append(parameter.getType().getSimpleName()).append(" ").append(parameter.getFieldName()).append("\n");
             }
+            sb.append("\n");
+            //
+            sb.append("public ").append(Util.ucFirst(this.callName)).append("Call(BergamotClient client)\n");
+            sb.append("{\n");
+            sb.append("    super(client);\n");
+            sb.append("}\n");
+            //
             sb.append("\n");
             for (RouteParameter parameter : this.parameters)
             {
@@ -165,7 +281,7 @@ public class AboutRouter extends Router<BergamotApp>
                 sb.append("    }\n");
             }
             //
-            String url = "\"" + (this.prefix.endsWith("/") ? this.prefix.substring(0, this.prefix.length() -1) : this.prefix) + this.pattern + "\"";
+            String url = "\"" + this.getPath() + "\"";
             for (RouteParameter param : this.parameters)
             {
                 if (param.isAs()) 
@@ -173,7 +289,7 @@ public class AboutRouter extends Router<BergamotApp>
             }
             //
             sb.append("\n");
-            sb.append("    public ").append(this.getReturnType().getSimpleName()).append(" execute()\n");
+            sb.append("    public ").append(this.getBindingReturnType()).append(" execute()\n");
             sb.append("    {\n");
             sb.append("        try\n");
             sb.append("        {\n");
@@ -222,7 +338,18 @@ public class AboutRouter extends Router<BergamotApp>
                 }
             }
             sb.append("            );\n");
-            sb.append("            return transcoder().decodeFromString(response.returnContent().asString(), ").append(this.getReturnType().getSimpleName()).append(".class);\n");
+            if (void.class == this.returnType)
+            {
+                sb.append("            return response.returnContent().asString();\n");
+            }
+            else if (List.class == this.returnType)
+            {
+                sb.append("            return transcoder().decodeListFromString(response.returnContent().asString(), ").append(this.returnOfType == null ? "?" : this.returnOfType.getSimpleName()).append(".class);\n");                
+            }
+            else
+            {
+                sb.append("            return transcoder().decodeFromString(response.returnContent().asString(), ").append(this.getReturnType().getSimpleName()).append(".class);\n");
+            }
             sb.append("        }\n");
             sb.append("        catch (IOException e)\n");
             sb.append("        {\n");
@@ -230,9 +357,14 @@ public class AboutRouter extends Router<BergamotApp>
             sb.append("        }\n");
             sb.append("    }\n");
             sb.append("\n");
-            //
             sb.append("}\n");
-            return sb.toString();
+            sb.append("\n\n");
+            // the binding method
+            sb.append("public ").append(Util.ucFirst(this.callName)).append("Call ").append(this.callName).append("()\n");
+            sb.append("{\n");
+            sb.append("    return new ").append(Util.ucFirst(this.callName)).append("Call(this);\n");
+            sb.append("}\n");
+            sb.append("\n");
         }
     }
     
