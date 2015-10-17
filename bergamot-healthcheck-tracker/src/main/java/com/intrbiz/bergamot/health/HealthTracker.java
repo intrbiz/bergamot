147e21f1534bf87c6f02ef90e79fb2e89f2145f6
Chris Ellis <chris@intrbiz.com>
2015-10-17 11:04:43 +0100

    Bergamot cluster health tracker

--- bergamot-healthcheck-tracker/src/main/java/com/intrbiz/bergamot/health/HealthTracker.java
+++ bergamot-healthcheck-tracker/src/main/java/com/intrbiz/bergamot/health/HealthTracker.java
@@ -1,6 +1,203 @@
 package com.intrbiz.bergamot.health;
 
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.TreeSet;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
+
+import org.apache.log4j.Logger;
+
+import com.intrbiz.bergamot.health.model.KnownDaemon;
+import com.intrbiz.bergamot.model.message.health.HealthCheckHeartbeat;
+import com.intrbiz.bergamot.model.message.health.HealthCheckJoin;
+import com.intrbiz.bergamot.model.message.health.HealthCheckMessage;
+import com.intrbiz.bergamot.model.message.health.HealthCheckRequestJoin;
+import com.intrbiz.bergamot.model.message.health.HealthCheckUnjoin;
+import com.intrbiz.bergamot.queue.HealthCheckQueue;
+import com.intrbiz.queue.Producer;
+import com.intrbiz.queue.name.NullKey;
+
 public class HealthTracker
 {
-
+    private static final HealthTracker US = new HealthTracker();
+    
+    public static HealthTracker getInstance()
+    {
+        return US;
+    }
+    
+    private ConcurrentMap<UUID, KnownDaemon> knownDaemons = new ConcurrentHashMap<UUID, KnownDaemon>();
+    
+    private volatile boolean inited = false;
+    
+    private HealthCheckQueue queue;
+    
+    @SuppressWarnings("unused")
+    private com.intrbiz.queue.Consumer<HealthCheckMessage, NullKey> healthcheckConsumer;
+    
+    private Producer<HealthCheckMessage> healthcheckProducer;
+    
+    private Logger logger = Logger.getLogger(HealthTracker.class);
+    
+    private Timer timer;
+    
+    private final CopyOnWriteArrayList<Consumer<KnownDaemon>> alertHandlers = new CopyOnWriteArrayList<Consumer<KnownDaemon>>();
+    
+    private HealthTracker()
+    {
+        super();
+    }
+    
+    public void init()
+    {
+        synchronized (this)
+        {
+            if (! this.inited)
+            {
+                this.inited = true;
+                // setup queues
+                this.setupQueue();
+                // request daemons to join
+                this.requestJoin();
+                // setup the check timer
+                this.setupTimer();
+            }
+        }
+    }
+    
+    public Set<KnownDaemon> getDaemons()
+    {
+        return new TreeSet<KnownDaemon>(this.knownDaemons.values());
+    }
+    
+    public KnownDaemon getDaemon(UUID instanceId)
+    {
+        return this.knownDaemons.get(instanceId);
+    }
+    
+    public void removeDaemon(UUID daemon)
+    {
+        this.knownDaemons.remove(daemon);
+    }
+    
+    public void addAlertHandler(Consumer<KnownDaemon> alertHandler)
+    {
+        this.alertHandlers.add(alertHandler);
+    }
+    
+    public void removeAlertHandler(Consumer<KnownDaemon> alertHandler)
+    {
+        this.alertHandlers.remove(alertHandler);
+    }
+    
+    private void setupTimer()
+    {
+        this.timer = new Timer();
+        this.timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run()
+            {
+                checkDaemons();
+            }
+        }, 
+        11_000L, 5_000L);
+    }
+    
+    private void checkDaemons()
+    {
+        for (KnownDaemon daemon : this.knownDaemons.values())
+        {
+            if (daemon.isAlive() && daemon.isLastHeartbeatTooOld())
+            {
+                // daemon was alive but we've missed enough heartbeats 
+                // for it to now be considered dead
+                daemon.setAlive(false);
+                daemon.incAlertCount();
+                daemon.setLastAlertTime(System.currentTimeMillis());
+                // raise alert
+                for (Consumer<KnownDaemon> handler : this.alertHandlers)
+                {
+                    handler.accept(daemon);
+                }
+            }
+        }
+    }
+    
+    private void setupQueue()
+    {
+        this.queue = HealthCheckQueue.open();
+        this.healthcheckProducer = this.queue.publishHealthCheckControlEvents();
+        this.healthcheckConsumer = this.queue.consumeHealthCheckEvents(this::handleMessage);
+    }
+    
+    private void handleMessage(HealthCheckMessage message)
+    {
+        if (message instanceof HealthCheckHeartbeat)
+        {
+            // heartbeat
+            this.processHeartbeat((HealthCheckHeartbeat) message);
+        }
+        else if (message instanceof HealthCheckJoin)
+        {
+            // join
+            this.processJoin((HealthCheckJoin) message);
+        }
+        else if (message instanceof HealthCheckUnjoin)
+        {
+            // unjoin
+            this.processUnjoin((HealthCheckUnjoin) message);
+        }
+    }
+    
+    private void processHeartbeat(HealthCheckHeartbeat heartbeat)
+    {
+        // lookup the daemon
+        KnownDaemon daemon = this.knownDaemons.get(heartbeat.getInstanceId());
+        if (daemon != null)
+        {
+            // update the state
+            daemon.setLastHeartbeatSequence(heartbeat.getSequence());
+            // use nanoTime as that is monotonic
+            daemon.setLastHeartbeatAt(System.nanoTime());
+            daemon.setLastHeartbeatTime(System.currentTimeMillis());
+            // recovery handling
+            if (! daemon.isAlive())
+            {
+                if (daemon.incRecoveryHeartbeatCount() >= 60)
+                {
+                    // we've have 5 minutes of successful heartbeats
+                    daemon.incRecoveryCount();
+                    daemon.setRecoveryHeartbeatCount(0);
+                    daemon.setAlive(true);
+                    daemon.setLastRecoveryTime(System.currentTimeMillis());
+                }
+            }
+        }
+        else
+        {
+            logger.warn("Got heartbeat for unkown daemon " + heartbeat.getInstanceId());
+        }
+    }
+    
+    private void processUnjoin(HealthCheckUnjoin unjoin)
+    {
+        this.knownDaemons.remove(unjoin.getInstanceId());
+        logger.info("Received unjoin event from " + unjoin.getInstanceId() + " daemon " + unjoin.getDaemonName());
+    }
+    
+    private void processJoin(HealthCheckJoin join)
+    {
+        this.knownDaemons.put(join.getInstanceId(), new KnownDaemon(join.getInstanceId(), join.getRuntimeId(), join.getDaemonName(), join.getStarted(), join.getHostName()));
+        logger.info("Received join event from " + join.getInstanceId() + " daemon " + join.getDaemonName());
+    }
+    
+    private void requestJoin()
+    {
+        this.healthcheckProducer.publish(new HealthCheckRequestJoin());
+    }
 }
---
+++ bergamot-healthcheck-tracker/src/main/java/com/intrbiz/bergamot/health/model/KnownDaemon.java
@@ -0,0 +1,225 @@
+package com.intrbiz.bergamot.health.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class KnownDaemon implements Comparable<KnownDaemon>
+{
+    private final UUID instanceId;
+    
+    private final UUID runtimeId;
+    
+    private final String daemonName;
+    
+    private final long startedAt;
+    
+    private final String hostName;
+    
+    private volatile long lastHeartbeatAt = System.nanoTime();
+    
+    private volatile long lastHeartbeatTime = System.currentTimeMillis();
+    
+    private volatile long lastHeartbeatSequence = 0;
+    
+    private volatile int alertCount = 0;
+    
+    private volatile int recoveryCount = 0;
+    
+    private volatile int recoveryHeartbeatCount = 0;
+    
+    private volatile long lastRecoveryTime = System.currentTimeMillis();
+    
+    private volatile long lastAlertTime = -1;
+    
+    private volatile boolean alive = true;
+    
+    public KnownDaemon(UUID instanceId, UUID runtimeId, String daemonName, long startedAt, String hostName)
+    {
+        Objects.requireNonNull(instanceId);
+        this.instanceId = instanceId;
+        this.runtimeId = runtimeId;
+        this.daemonName = daemonName;
+        this.startedAt = startedAt;
+        this.hostName = hostName;
+    }
+
+    public long getLastHeartbeatAt()
+    {
+        return lastHeartbeatAt;
+    }
+
+    public void setLastHeartbeatAt(long lastHeartbeatAt)
+    {
+        this.lastHeartbeatAt = lastHeartbeatAt;
+    }
+
+    public long getLastHeartbeatSequence()
+    {
+        return lastHeartbeatSequence;
+    }
+
+    public void setLastHeartbeatSequence(long lastHeartbeatSequence)
+    {
+        this.lastHeartbeatSequence = lastHeartbeatSequence;
+    }
+
+    public UUID getInstanceId()
+    {
+        return instanceId;
+    }
+    
+    public UUID getRuntimeId()
+    {
+        return this.runtimeId;
+    }
+
+    public String getDaemonName()
+    {
+        return daemonName;
+    }
+
+    public long getStartedAt()
+    {
+        return startedAt;
+    }
+
+    public String getHostName()
+    {
+        return hostName;
+    }
+
+    public long getLastHeartbeatTime()
+    {
+        return lastHeartbeatTime;
+    }
+
+    public void setLastHeartbeatTime(long lastHeartbeatTime)
+    {
+        this.lastHeartbeatTime = lastHeartbeatTime;
+    }
+
+    public int getAlertCount()
+    {
+        return alertCount;
+    }
+
+    public void setAlertCount(int alertCount)
+    {
+        this.alertCount = alertCount;
+    }
+    
+    public int incAlertCount()
+    {
+        this.alertCount++;
+        return this.alertCount;
+    }
+
+    public long getLastAlertTime()
+    {
+        return lastAlertTime;
+    }
+
+    public void setLastAlertTime(long lastAlertTime)
+    {
+        this.lastAlertTime = lastAlertTime;
+    }
+    
+    /**
+     * How long ago was the last heartbeat
+     * @return the number of milliseconds since the last heartbeat
+     */ 
+    public long getLastHeartbeatAge()
+    {
+        return (System.nanoTime() - this.lastHeartbeatAt) / 1_000_000L;
+    }
+
+    public boolean isLastHeartbeatTooOld()
+    {
+        // how long ago was the last heartbeat
+        long timeSinceLastHeartbeat = this.lastHeartbeatAt - System.nanoTime();
+        // was it too long (over 30 seconds) since the last heartbeat
+        return timeSinceLastHeartbeat < 30_000_000_000L;
+    }
+
+    public boolean isAlive()
+    {
+        return alive;
+    }
+
+    public void setAlive(boolean alive)
+    {
+        this.alive = alive;
+    }
+
+    public int getRecoveryCount()
+    {
+        return recoveryCount;
+    }
+
+    public void setRecoveryCount(int recoveryCount)
+    {
+        this.recoveryCount = recoveryCount;
+    }
+    
+    public int incRecoveryCount()
+    {
+        this.recoveryCount ++;
+        return this.recoveryCount;
+    }
+
+    public int getRecoveryHeartbeatCount()
+    {
+        return recoveryHeartbeatCount;
+    }
+
+    public void setRecoveryHeartbeatCount(int recoveryHeartbeatCount)
+    {
+        this.recoveryHeartbeatCount = recoveryHeartbeatCount;
+    }
+    
+    public int incRecoveryHeartbeatCount()
+    {
+        this.recoveryHeartbeatCount++;
+        return recoveryHeartbeatCount;
+    }
+
+    @Override
+    public int compareTo(KnownDaemon o)
+    {
+        return this.instanceId.compareTo(o.instanceId);
+    }
+
+    public long getLastRecoveryTime()
+    {
+        return lastRecoveryTime;
+    }
+
+    public void setLastRecoveryTime(long lastRecoveryTime)
+    {
+        this.lastRecoveryTime = lastRecoveryTime;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((instanceId == null) ? 0 : instanceId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        KnownDaemon other = (KnownDaemon) obj;
+        if (instanceId == null)
+        {
+            if (other.instanceId != null) return false;
+        }
+        else if (!instanceId.equals(other.instanceId)) return false;
+        return true;
+    }
+}
