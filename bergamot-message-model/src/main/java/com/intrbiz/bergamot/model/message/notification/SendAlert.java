412ac386016472f18e9ccd237b072aba4afebdd1
Chris Ellis <chris@intrbiz.com>
2015-11-13 22:08:14 +0000

    Add escalation flag to alert notifications

--- bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/notification/SendAlert.java
+++ bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/notification/SendAlert.java
@@ -1,5 +1,6 @@
 package com.intrbiz.bergamot.model.message.notification;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 /**
@@ -8,6 +9,12 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @JsonTypeName("bergamot.send_alert")
 public class SendAlert extends CheckNotification
 {
+    /**
+     * Is this alert notification an escalation of a previous alert
+     */
+    @JsonProperty("escalation")
+    private boolean escalation = false;
+    
     public SendAlert()
     {
         super();
@@ -16,5 +23,15 @@ public class SendAlert extends CheckNotification
     public String getNotificationType()
     {
         return "alert";
+    }
+
+    public boolean isEscalation()
+    {
+        return escalation;
+    }
+
+    public void setEscalation(boolean escalation)
+    {
+        this.escalation = escalation;
     }
 }
