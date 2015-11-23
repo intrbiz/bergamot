c2914486a60d4db464aeac0e43c978e0c91e3a59
Chris Ellis <chris@intrbiz.com>
2015-11-23 09:15:59 +0000

    Improve ui flag icons
    Improve UI support for escalations and check dependencie

--- bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/DashboardRouter.java
+++ bergamot-ui/src/main/java/com/intrbiz/bergamot/ui/router/DashboardRouter.java
@@ -1,13 +1,13 @@
 package com.intrbiz.bergamot.ui.router;
 
-import static com.intrbiz.bergamot.ui.util.Sorter.*;
+import static com.intrbiz.bergamot.ui.util.Sorter.orderGroupsByStatus;
+import static com.intrbiz.bergamot.ui.util.Sorter.orderLocationsByStatus;
 
 import java.util.stream.Collectors;
 
 import com.intrbiz.balsa.engine.route.Router;
 import com.intrbiz.balsa.metadata.WithDataAdapter;
 import com.intrbiz.bergamot.data.BergamotDB;
-import com.intrbiz.bergamot.model.Alert;
 import com.intrbiz.bergamot.model.Site;
 import com.intrbiz.bergamot.ui.BergamotApp;
 import com.intrbiz.metadata.Any;
@@ -25,7 +25,7 @@ public class DashboardRouter extends Router<BergamotApp>
     @WithDataAdapter(BergamotDB.class)
     public void index(BergamotDB db, @SessionVar("site") Site site)
     {
-        model("alerts", db.listAlerts(site.getId()).stream().map(Alert::getCheck).filter((c) -> permission("read", c)).collect(Collectors.toList()));
+        model("alerts", db.listAlerts(site.getId()).stream().filter((a) -> permission("read", a.getCheckId())).collect(Collectors.toList()));
         model("groups", orderGroupsByStatus(permission("read", db.getRootGroups(site.getId()))));
         model("locations", orderLocationsByStatus(permission("read", db.getRootLocations(site.getId()))));
         encode("index");
