b6495c419bbb485200ff85c7ec751b2b17b9739f
Chris Ellis <chris@intrbiz.com>
2016-01-31 15:39:11 +0000

    Package up SSH worker

--- bergamot-worker-ssh/src/main/java/com/intrbiz/bergamot/worker/SSHWorker.java
+++ bergamot-worker-ssh/src/main/java/com/intrbiz/bergamot/worker/SSHWorker.java
@@ -5,13 +5,13 @@ import com.intrbiz.bergamot.worker.Worker;
 import com.intrbiz.bergamot.worker.config.SSHWorkerCfg;
 
 /**
- * A worker to execute nagios check engines
+ * A worker to execute SSH check engines
  */
 public class SSHWorker extends DefaultWorker
 {
     public SSHWorker()
     {
-        super(SSHWorkerCfg.class, "/etc/bergamot/worker/nagios.xml", "bergamot-worker-nagios");
+        super(SSHWorkerCfg.class, "/etc/bergamot/worker/ssh.xml", "bergamot-worker-ssh");
     }
     
     public static void main(String[] args) throws Exception
