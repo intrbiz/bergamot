7fb243cfb52bf19619d8b139e43c8907bd5d6e43
Chris Ellis <chris@intrbiz.com>
2016-02-06 17:12:35 +0000

    Correct type name

--- bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/api/check/ExecuteAdhocCheck.java
+++ bergamot-message-model/src/main/java/com/intrbiz/bergamot/model/message/api/check/ExecuteAdhocCheck.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.intrbiz.bergamot.model.message.api.APIRequest;
 import com.intrbiz.bergamot.model.message.check.ExecuteCheck;
 
-@JsonTypeName("bergamot.api.bergamot.api.execute_adhoc_check")
+@JsonTypeName("bergamot.api.execute_adhoc_check")
 public class ExecuteAdhocCheck extends APIRequest
 {
     @JsonProperty("check")
