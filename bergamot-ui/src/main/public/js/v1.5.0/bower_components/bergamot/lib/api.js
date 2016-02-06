64960a90751c542ca9e4b93de54d7ea7706d8e2f
Chris Ellis <chris@intrbiz.com>
2016-02-06 17:17:26 +0000

    Add API bindings for new calls

--- bergamot-ui/src/main/public/js/v1.5.0/bower_components/bergamot/lib/api.js
+++ bergamot-ui/src/main/public/js/v1.5.0/bower_components/bergamot/lib/api.js
@@ -8,8 +8,11 @@ define(function (require)
             if (! onResponse) { onResponse = function(message) {} }
             // if onError is not provided use onResponse
             if (! onError) { onError = onResponse; }
-            // send the ping
-            this.trigger("bergamot-api-ping", { "onResponse": onResponse.bind(this), "onError": onError.bind(this) });
+            // send
+            this.trigger("bergamot-api-ping", { 
+                "onResponse": onResponse.bind(this), 
+                "onError": onError.bind(this)
+            });
         };
         
         this.registerForUpdates = function(/*String*/ updateType, /*Array*/ ids, /*Function*/ onResponse, /*Function*/ onError)
@@ -18,8 +21,13 @@ define(function (require)
             if (! onResponse) { onResponse = function(message) {} }
             // if onError is not provided use onResponse
             if (! onError) { onError = onResponse; }
-            // send the ping
-            this.trigger("bergamot-api-register-for-updates", { "update_type": updateType, "ids": ids, "onResponse": onResponse.bind(this), "onError": onError.bind(this) });
+            // send
+            this.trigger("bergamot-api-register-for-updates", { 
+                "update_type": updateType, 
+                "ids": ids, 
+                "onResponse": onResponse.bind(this), 
+                "onError": onError.bind(this)
+            });
         };
         
         this.registerForNotifications = function(/*String*/ siteId, /*Function*/ onResponse, /*Function*/ onError)
@@ -28,8 +36,43 @@ define(function (require)
             if (! onResponse) { onResponse = function(message) {} }
             // if onError is not provided use onResponse
             if (! onError) { onError = onResponse; }
-            // send the ping
-            this.trigger("bergamot-api-register-for-notifications", { "site_id": siteId, "onResponse": onResponse.bind(this), "onError": onError.bind(this) });
+            // send
+            this.trigger("bergamot-api-register-for-notifications", { 
+                "site_id": siteId, 
+                "onResponse": onResponse.bind(this), 
+                "onError": onError.bind(this) 
+            });
+        };
+        
+        this.registerForAdhocResults = function(/*Function*/ onResponse, /*Function*/ onError)
+        {
+            // a default onResponse handler
+            if (! onResponse) { onResponse = function(message) {} }
+            // if onError is not provided use onResponse
+            if (! onError) { onError = onResponse; }
+            // send
+            this.trigger("bergamot-api-register-for-adhoc-results", { 
+                "onResponse": onResponse.bind(this), 
+                "onError": onError.bind(this) 
+            });
+        };
+        
+        this.executeAdhocCheck = function(/*Object*/ check, /*Object*/ opts, /*Function*/ onResponse, /*Function*/ onError)
+        {
+            // a default onResponse handler
+            if (! onResponse) { onResponse = function(message) {} }
+            // if onError is not provided use onResponse
+            if (! onError) { onError = onResponse; }
+            // send
+            this.trigger("bergamot-api-execute-adhoc-check", { 
+                "check": check, 
+                "worker_pool": opts.worker_pool, 
+                "engine": opts.engine,
+                "agent_id": opts.agent_id, 
+                "ttl": opts.ttl,
+                "onResponse": onResponse.bind(this), 
+                "onError": onError.bind(this) 
+            });
         };
     };
 });
