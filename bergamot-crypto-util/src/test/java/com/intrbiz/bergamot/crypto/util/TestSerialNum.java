9498124a5a63359c23a38a9bea6415212241ad8d
Chris Ellis <chris@intrbiz.com>
2016-02-25 00:31:54 +0000

    Add some more tests

--- bergamot-crypto-util/src/test/java/com/intrbiz/bergamot/crypto/util/TestSerialNum.java
+++ bergamot-crypto-util/src/test/java/com/intrbiz/bergamot/crypto/util/TestSerialNum.java
@@ -109,4 +109,26 @@ public class TestSerialNum
         assertThat(sn1, is(equalTo(sn2)));
         assertThat(bi, is(equalTo(sn2.toBigInt())));
     }
+    
+    @Test()
+    public void testSerialNumV2()
+    {
+        UUID id = UUID.randomUUID();
+        short rev = 16;
+        short mode = SerialNum.MODE_TEMPLATE;
+        // create our serial
+        SerialNum sn1 = SerialNum.version2(id, rev, mode);
+        assertThat(sn1.getId(), is(equalTo(id)));
+        assertThat(sn1.getRev(), is(equalTo(rev)));
+        assertThat(sn1.getMode(), is(equalTo(mode)));
+        assertThat(sn1.isVersion2(), is(equalTo(true)));
+        assertThat(sn1.isTemplate(), is(equalTo(true)));
+        // encode
+        BigInteger bi = sn1.toBigInt();
+        // decode
+        SerialNum sn2 = SerialNum.fromBigInt(bi);
+        // assert they match
+        assertThat(sn1, is(equalTo(sn2)));
+        assertThat(bi, is(equalTo(sn2.toBigInt())));
+    }
 }
