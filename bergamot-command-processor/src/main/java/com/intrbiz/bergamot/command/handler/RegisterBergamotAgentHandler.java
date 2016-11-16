550b1a515e17277609e5879148f6ace26b6d0bbf
Chris Ellis <chris@intrbiz.com>
2016-11-16 20:02:10 +0000

    Create host on agent register

--- bergamot-command-processor/src/main/java/com/intrbiz/bergamot/command/handler/RegisterBergamotAgentHandler.java
+++ bergamot-command-processor/src/main/java/com/intrbiz/bergamot/command/handler/RegisterBergamotAgentHandler.java
@@ -2,6 +2,7 @@ package com.intrbiz.bergamot.command.handler;
 
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.sql.Timestamp;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -9,11 +10,17 @@ import org.apache.log4j.Logger;
 import com.intrbiz.accounting.Accounting;
 import com.intrbiz.bergamot.accounting.model.SignAgentAccountingEvent;
 import com.intrbiz.bergamot.command.CommandProcessor;
+import com.intrbiz.bergamot.config.model.BergamotCfg;
+import com.intrbiz.bergamot.config.model.HostCfg;
+import com.intrbiz.bergamot.config.validator.ValidatedBergamotConfiguration;
 import com.intrbiz.bergamot.crypto.util.PEMUtil;
 import com.intrbiz.bergamot.crypto.util.SerialNum;
 import com.intrbiz.bergamot.data.BergamotDB;
+import com.intrbiz.bergamot.importer.BergamotConfigImporter;
+import com.intrbiz.bergamot.importer.BergamotImportReport;
 import com.intrbiz.bergamot.model.AgentRegistration;
 import com.intrbiz.bergamot.model.Config;
+import com.intrbiz.bergamot.model.ConfigChange;
 import com.intrbiz.bergamot.model.Site;
 import com.intrbiz.bergamot.model.message.agent.manager.AgentManagerRequest;
 import com.intrbiz.bergamot.model.message.agent.manager.AgentManagerResponse;
@@ -57,6 +64,9 @@ public class RegisterBergamotAgentHandler implements BergamotCommandHandler<Regi
                 if (site == null) throw new RuntimeException("No such site");
                 Config template = db.getConfig(request.getAgentId());
                 if (template == null) throw new RuntimeException("No such template");
+                HostCfg hostTemplate = (HostCfg) template.getConfiguration();
+                // assert that the template is a Host template
+                if (! (template.getConfiguration() instanceof HostCfg)) throw new RuntimeException("The template must be a host template");
                 // call to the agent manager to sign the certificate
                 AgentManagerResponse response = this.client.publish(new SignAgent(site.getId(), request.getAgentId(), request.getCommonName(), request.getPublicKey())).get(10, TimeUnit.SECONDS);
                 if (response instanceof SignedAgent)
@@ -67,7 +77,9 @@ public class RegisterBergamotAgentHandler implements BergamotCommandHandler<Regi
                     this.accounting.account(new SignAgentAccountingEvent(site.getId(), request.getAgentId(), request.getCommonName(), ((X509Certificate) crt).getSerialNumber().toString(), null));
                     // store the certificate
                     db.setAgentRegistration(new AgentRegistration(site.getId(), request.getAgentId(), request.getCommonName(), SerialNum.fromBigInt(((X509Certificate) crt).getSerialNumber()).toString()));
-                    // response
+                    // create a host based on the template
+                    this.applyConfigurationChange(db, request, site, hostTemplate);
+                    // got a certificate and registered the host
                     RegisteredBergamotAgent resp = new RegisteredBergamotAgent();
                     resp.setAgentId(request.getAgentId());
                     resp.setCertificate(((SignedAgent) response).getCertificatePEM());
@@ -83,6 +95,49 @@ public class RegisterBergamotAgentHandler implements BergamotCommandHandler<Regi
         catch (Exception e)
         {
             return new GeneralCommandError(e.getMessage());
+        }
+    }
+    
+    private void applyConfigurationChange(BergamotDB db, RegisterBergamotAgent request, Site site, HostCfg template)
+    {
+        // create the configuration change
+        BergamotCfg changeCfg = new BergamotCfg();
+        changeCfg.setSummary("Auto register host: " + request.getCommonName());
+        // the host we are adding
+        HostCfg host = new HostCfg();
+        host.setAgentId(request.getAgentId());
+        host.setName(request.getCommonName());
+        host.getInheritedTemplates().add(template.getName());
+        changeCfg.addObject(host);
+        // log the config change
+        ConfigChange change = new ConfigChange(site.getId(), null, changeCfg);
+        db.setConfigChange(change);
+        // apply it
+        ValidatedBergamotConfiguration validatedConfiguration = changeCfg.validate(db.getObjectLocator(site.getId()));
+        if (validatedConfiguration.getReport().isValid())
+        {
+            // good we have a valid configuration, import it
+            BergamotConfigImporter importer = new BergamotConfigImporter(validatedConfiguration).online(true);
+            BergamotImportReport importReport = importer.importConfiguration();
+            if (importReport.isSuccessful())
+            {
+                // whoop, we successfully registered the host
+                change.setApplied(true);
+                change.setAppliedAt(new Timestamp(System.currentTimeMillis()));
+                db.setConfigChange(change);
+            }
+            else
+            {
+                logger.error("Failed to auto register host, the configuration import failed");
+                // keep the config change, it can be manually applied later
+                // TODO: raise a notifcation in this instance    
+            }
+        }
+        else
+        {
+            logger.error("Failed to auto register host, the resultant configuration is not valid");
+            // keep the config change, it can be manually applied later
+            // TODO: raise a notifcation in this instance
         }
     }
 
