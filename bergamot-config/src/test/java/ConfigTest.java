b68f1ed042929a122f9b6d6cbfea59abb3e1501b
Chris Ellis <chris@intrbiz.com>
2017-11-25 23:54:07 +0000

    Tweak config test

--- bergamot-config/src/test/java/ConfigTest.java
+++ bergamot-config/src/test/java/ConfigTest.java
@@ -10,15 +10,17 @@ public class ConfigTest
 {
     public static void main(String[] args) throws Exception
     {
-        for (ValidatedBergamotConfiguration cfg : new BergamotConfigReader().includeDir(new File("../cfg/local/")).build())
+        for (ValidatedBergamotConfiguration cfg : new BergamotConfigReader().includeDir(new File("../bergamot-site-config-template/")).build())
         {
-            System.out.println(cfg.toString());
-            new BergamotConfigWriter().baseDir(new File(new File("../cfg/" ), cfg.getConfig().getSite())).config(cfg.getConfig()).write();
+            // write out the config
+            // new BergamotConfigWriter().baseDir(new File(new File("../cfg/" ), cfg.getConfig().getSite())).config(cfg.getConfig()).write();
             // print the resolved config
             for (HostCfg host : cfg.getConfig().getHosts())
             {
                 System.out.println(host.resolve());
             }
+            // validation report
+            System.out.println(cfg.getReport());
         }
     }
 }
