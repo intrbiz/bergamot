b8972f30927a8c549f893d50e30f683693831fd0
Chris Ellis <chris@intrbiz.com>
2020-03-11 23:48:13 +0000

    Remove deprecated modules

--- bergamot-agent-manager/src/main/java/com/intrbiz/bergamot/agent/manager/config/BergamotKeyStoreCfg.java
+++
@@ -1,8 +0,0 @@
-package com.intrbiz.bergamot.agent.manager.config;
-
-import com.intrbiz.configuration.Configuration;
-
-public abstract class BergamotKeyStoreCfg extends Configuration
-{
-    private static final long serialVersionUID = 1L;
-}
