3dcbfcedf64b5f2e8802958274abf3d1d562da25
Chris Ellis <chris@intrbiz.com>
2020-03-17 04:22:23 +0000

    Tweak CI

--- containers/build_functions.sh
+++ containers/build_functions.sh
@@ -3,24 +3,24 @@
 function docker_build_util {
     NAME=$1
     echo "Building util container $NAME"
-    pushd $NAME >/dev/null
+    cd $NAME
     docker build --no-cache $DOCKER_OPTS -t $NAME:latest -t bergamotmonitoring/$NAME:latest .
     docker push bergamotmonitoring/$NAME:latest
-    popd >/dev/null
+    cd ..
 }
 
 function docker_build_app {
     NAME=$1
     BERGAMOT_VERSION=$2
     echo "Building application container $NAME version $BERGAMOT_VERSION"
-    pushd $NAME >/dev/null
+    cd $NAME
     docker build --no-cache $DOCKER_OPTS --build-arg bergamot_version=$BERGAMOT_VERSION -t $NAME:$BERGAMOT_VERSION -t bergamotmonitoring/$NAME:$BERGAMOT_VERSION .
     docker push bergamotmonitoring/$NAME:$BERGAMOT_VERSION
-    popd >/dev/null
+    cd ..
 }
 
 function get_app_version {
-    pushd .. >/dev/null
+    cd ..
     echo $(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-    popd >/dev/null
+    cd containers
 }
