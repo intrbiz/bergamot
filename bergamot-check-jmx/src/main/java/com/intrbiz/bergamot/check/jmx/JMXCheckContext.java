33b563df407091ec86b60e22f8d0a9ca6a1a8349
Chris Ellis <chris@intrbiz.com>
2020-05-29 00:57:15 +0100

    Improve JMX Support

--- bergamot-check-jmx/src/main/java/com/intrbiz/bergamot/check/jmx/JMXCheckContext.java
+++ bergamot-check-jmx/src/main/java/com/intrbiz/bergamot/check/jmx/JMXCheckContext.java
@@ -1,10 +1,14 @@
 package com.intrbiz.bergamot.check.jmx;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
+
+import com.intrbiz.Util;
 
 public class JMXCheckContext
 {    
@@ -15,12 +19,12 @@ public class JMXCheckContext
         this.onError = onError;
     }
     
-    public void connect(String url, Consumer<JMXConnection> onConnected)
+    public void connect(String url, Map<String,?> env, Consumer<JMXConnection> onConnected)
     {
         try
         {
             JMXServiceURL jmxUrl = new JMXServiceURL(url);
-            try (JMXConnector jmxc = JMXConnectorFactory.connect(jmxUrl))
+            try (JMXConnector jmxc = JMXConnectorFactory.connect(jmxUrl, env))
             {
                 onConnected.accept(new JMXConnection(jmxc.getMBeanServerConnection()));
             }
@@ -31,8 +35,40 @@ public class JMXCheckContext
         }
     }
     
+    public void connect(String url, String username, String password, Consumer<JMXConnection> onConnected)
+    {
+        this.connect(url, buildEnv(username, password), onConnected);
+    }
+    
+    public void connect(String host, int port, Map<String, ?> env, Consumer<JMXConnection> onConnected)
+    {
+        this.connect("service:jmx:rmi:///jndi/rmi://"+ host + ":" + port + "/jmxrmi", env, onConnected);
+    }
+    
     public void connect(String host, int port, Consumer<JMXConnection> onConnected)
     {
-        this.connect("service:jmx:rmi:///jndi/rmi://"+ host + ":" + port + "/jmxrmi", onConnected);
+        this.connect(host, port, null, onConnected);
+    }
+    
+    public void connect(String host, int port, String username, String password, Consumer<JMXConnection> onConnected)
+    {
+        
+        this.connect(host, port, buildEnv(username, password), onConnected);
+    }
+    
+    public Map<String, Object> buildEnv(String username, String password)
+    {
+        Map<String, Object> env = this.buildEnv();
+        if (! Util.isEmpty(username))
+        {
+            env.put(JMXConnector.CREDENTIALS, new String[] { username, password });
+        }
+        return env;
+    }
+    
+    public Map<String, Object> buildEnv()
+    {
+        Map<String, Object> env = new HashMap<>();
+        return env;
     }
 }
